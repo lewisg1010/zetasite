4775b5a3b3f74768c79410d989b3f8a4edf4e85e
Galen Lewis <galen@spotr.fit>
2023-02-09 13:59:11 -0500

    responsiveness improvements

--- src/App.js
+++ src/App.js
@@ -46,24 +46,33 @@ function App() {
                     </form>
                     
                     <Row xs={2} sm={2} md={2} lg={2} xl={2} className="g-5 align-items-center">
-                        <Col >
+                        <Col id="col2">
+
+                            <div id="valueprop">
                             <QuestionAnswerIcon />
                             <p className="heading">Communications Hub</p>
                             <p className="body">Built-in phone, text, and email capabilities let you talk to your customers on their preferred channel, all from one CRM.</p>
+                            </div>
 
+                            <div id="valueprop">
                             <AssistantIcon />
                             <p className="heading">Personalized, 24/7 Assistant</p>
-                            <p className="body">Zeta answers calls, texts, emails, and chats across all your marketing channels, 24/7. Plus, Zeta automatically follows up with prospects—saving your team precious time.</p>
+                            <p className="body">Zeta answers calls, texts, emails, and chats across all your marketing channels, and automatically follows up with prospects.</p>
+                            </div>
                         </Col>
                     
-                        <Col>
+                        <Col id="col2">
+                            <div id="valueprop">
                             <ScheduleIcon />
                             <p className="heading">Schedule Showings on Autopilot</p>
                             <p className="body">Let Zeta schedule your showings, so you can focus your time on what matters most: closing deals.</p>
-                   
+                            </div>
+
+                            <div id="valueprop">
                             <ShareIcon />
                             <p className="heading">Uses Your Property Database</p>
                             <p className="body">Since Zeta connects directly to your YGL, it can quickly send clients property listings, and answer property-specific questions.</p>
+                            </div>
                         </Col>
                     </Row>
                 </Col>
